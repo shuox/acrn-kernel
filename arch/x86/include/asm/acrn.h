ad4f667c19c743f29b34f4340d71f265db01e16c
Zhao Yakui <yakui.zhao@intel.com>
2019-08-16 09:45:17 +0800

    x86/acrn: Add hypercall for ACRN guest
    Guests running under the ACRN hypervisor use the VMCALL instruction
    to make hypercalls.
    
    Implement the ABI to allow those hypercalls to be made.
    
    For hypercall, ACRN has following assumptions:
    - Only 64bit guest could make hypercall
    - ACRN hypercall follows ACRN hypercall ABI:
      Hyercall number is in r8 register
      Up to 2 parameters are in RDI and RSI registers.
      Return value is in RAX register
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>
    Cc: Shuo A Liu <shuo.a.liu@intel.com>
    Cc: fengguang.wu@intel.com
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>

--- arch/x86/include/asm/acrn.h
+++ arch/x86/include/asm/acrn.h
@@ -20,4 +20,76 @@ extern void acrn_hv_vector_handler(struct pt_regs *regs);
 extern void acrn_setup_intr_irq(void (*handler)(void));
 extern void acrn_remove_intr_irq(void);
 extern bool acrn_is_privilege_vm(void);
+
+/*
+ * Hypercalls for ACRN guest
+ *
+ * - ACRN only allows 64bit guest make hypercall.
+ * - VMCALL instruction is used to implement ACRN hypercall.
+ * - This is ACRN hypercall ABI:
+ *   - Hypercall number is passed in R8 register.
+ *   - Up to 2 arguments are passed in RDI, RSI.
+ *   - Return value will be placed in RAX.
+ *
+ * NOTE: For ACRN hypercalls, the hypercall number need be
+ *       be accessed fast, we use register for it.
+ *
+ *       For some ACRN hypercalls, they need access relative
+ *       guest id fast, we use second register for it.
+ *
+ *       Other info will be passed to hypervisor by guest memory
+ *       copy. We use third register for guest memory address.
+ */
+static inline long acrn_hypercall0(unsigned long hcall_id)
+{
+	long result;
+
+	/*
+	 * The hypercall is implemented with the VMCALL instruction.
+	 * volatile qualifier is added to avoid that it is dropped
+	 * because of compiler optimization.
+	 *
+	 * "movq" is explicitly used to emphasize that ACRN only allows
+	 * 64bit guest make hypercall when "mov" also work here.
+	 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id)
+		     : "r8");
+
+	return result;
+}
+
+static inline long acrn_hypercall1(unsigned long hcall_id,
+				   unsigned long param1)
+{
+	long result;
+
+	/* See comments in acrn_hypercall0 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id), "D" (param1)
+		     : "r8");
+
+	return result;
+}
+
+static inline long acrn_hypercall2(unsigned long hcall_id,
+				   unsigned long param1,
+				   unsigned long param2)
+{
+	long result;
+
+	/* See comments in acrn_hypercall0 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id), "D" (param1), "S" (param2)
+		     : "r8");
+
+	return result;
+}
+
 #endif /* _ASM_X86_ACRN_H */
