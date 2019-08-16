007e57321aa14ef4cab28fc7df4eb0bf1f9cad08
Zhao Yakui <yakui.zhao@intel.com>
2019-08-16 09:45:17 +0800

    x86/acrn: Introduce hypercall interfaces
    The Service VM uses the VMCALL instruction to make hypercalls.
    
    ACRN hypercall ABI:
      * Hypercall number is in R8 register.
      * Up to 2 parameters are in RDI and RSI registers.
      * Return value is in RAX register.
    
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Fengwei Yin <fengwei.yin@intel.com>
    Cc: Zhi Wang <zhi.a.wang@intel.com>
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Cc: Yu Wang <yu1.wang@intel.com>
    
    ---
    v8:
      1) move a commit change to previous patch
    v5:
      1) Fix typos. (Dave)
      2) Refine comments to show ACRN hypercall ABI  (Dave)
      3) Comment explicit mov in asm. (Dave)
    ---

--- arch/x86/include/asm/acrn.h
+++ arch/x86/include/asm/acrn.h
@@ -13,4 +13,68 @@
 extern void acrn_setup_intr_irq(void (*handler)(void));
 extern void acrn_remove_intr_irq(void);
 extern bool acrn_is_privileged_vm(void);
+
+/*
+ * Hypercalls for ACRN
+ *
+ * - VMCALL instruction is used to implement ACRN hypercalls.
+ * - ACRN hypercall ABI:
+ *   - Hypercall number is passed in R8 register.
+ *   - Up to 2 arguments are passed in RDI, RSI.
+ *   - Return value will be placed in RAX.
+ */
+static inline long acrn_hypercall0(unsigned long hcall_id)
+{
+	long result;
+
+	/*
+	 * Use explicit MOV because gcc doesn't support R8
+	 * as direct register constraints.
+	 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id)
+		     : "r8");
+
+	return result;
+}
+
+static inline long acrn_hypercall1(unsigned long hcall_id,
+				   unsigned long param1)
+{
+	long result;
+
+	/*
+	 * Use explicit MOV because gcc doesn't support R8
+	 * as direct register constraints.
+	 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id), "D" (param1)
+		     : "r8");
+
+	return result;
+}
+
+static inline long acrn_hypercall2(unsigned long hcall_id,
+				   unsigned long param1,
+				   unsigned long param2)
+{
+	long result;
+
+	/*
+	 * Use explicit MOV because gcc doesn't support R8
+	 * as direct register constraints.
+	 */
+	asm volatile("movq %[hcall_id], %%r8\n\t"
+		     "vmcall\n\t"
+		     : "=a" (result)
+		     : [hcall_id] "g" (hcall_id), "D" (param1), "S" (param2)
+		     : "r8");
+
+	return result;
+}
+
 #endif /* _ASM_X86_ACRN_H */
