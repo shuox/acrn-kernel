33d1de898a4f26670e7f994c1f65c23f669e64b4
Yin Fengwei <fengwei.yin@intel.com>
2019-11-20 23:31:27 +0800

    x86/acrn: Add an API to check if a guest VM is privileged
    ACRN hypervisor reports hypervisor features by CPUID leaf 0x40000001
    which is similar with KVM. A guest VM can check if it's the privileged
    VM by the feature bits. The Service VM is the only privileged guest
    VM by the design.
    
    Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Fengwei Yin <fengwei.yin@intel.com>
    Cc: Yakui Zhao <yakui.zhao@intel.com>
    Cc: Zhi Wang <zhi.a.wang@intel.com>
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Cc: Yu Wang <yu1.wang@intel.com>
    ---
    v8:
      1) move one commit change from the next patch into this one(Zhi)
      2) uint32_t -> u32 (Zhi)
    v5:
      1) 'privilege VM' -> 'privileged VM', also fix it in code. (Dave)
      2) Align nomenclature 'Service VM' with existing documentation in
         previous patches. (Dave)
      3) Refine comments in code
    ---

--- arch/x86/include/asm/acrn.h
+++ arch/x86/include/asm/acrn.h
@@ -2,7 +2,15 @@
 #ifndef _ASM_X86_ACRN_H
 #define _ASM_X86_ACRN_H
 
+/*
+ * This CPUID returns feature bitmaps in EAX.
+ * Guest VM uses this to detect the appropriate feature bit.
+ */
+#define	ACRN_CPUID_FEATURES	0x40000001
+/* Bit 0 indicates whether guest VM is privileged */
+#define	ACRN_FEATURE_PRIVILEGED_VM	BIT(0)
+
 extern void acrn_setup_intr_irq(void (*handler)(void));
 extern void acrn_remove_intr_irq(void);
-
+extern bool acrn_is_privileged_vm(void);
 #endif /* _ASM_X86_ACRN_H */
--- arch/x86/kernel/cpu/acrn.c
+++ arch/x86/kernel/cpu/acrn.c
@@ -18,9 +18,31 @@
 #include <asm/idtentry.h>
 #include <asm/irq_regs.h>
 
-static uint32_t __init acrn_detect(void)
+static u32 __acrn_cpuid_base(void)
 {
 	return hypervisor_cpuid_base("ACRNACRNACRN\0\0", 0);
+}
+
+static u32 acrn_cpuid_base(void)
+{
+	static uint32_t acrn_cpuid_base = 0xFFFFFFFF;
+
+	if (acrn_cpuid_base == 0xFFFFFFFF)
+		acrn_cpuid_base = __acrn_cpuid_base();
+
+	return acrn_cpuid_base;
+}
+
+bool acrn_is_privileged_vm(void)
+{
+	return cpuid_eax(acrn_cpuid_base() | ACRN_CPUID_FEATURES) &
+			ACRN_FEATURE_PRIVILEGED_VM;
+}
+EXPORT_SYMBOL(acrn_is_privileged_vm);
+
+static u32 __init acrn_detect(void)
+{
+	return acrn_cpuid_base();
 }
 
 static void __init acrn_init_platform(void)
